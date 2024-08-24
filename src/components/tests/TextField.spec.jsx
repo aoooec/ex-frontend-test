1e697fe079e36d7efb0223cef362a59f9b365ccb
aoooec <foottable@gmail.com>
2024-08-25 00:05:03 +0900

    fix: test 오류 수정, describe 함수로 placeholder 테스트 그룹핑

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -10,19 +10,22 @@ import render from '@/utils/test/render';
 // test 함수: if~, ~~할 경우 ~~ 한다
 // describe 함수: 테스트를 그룹핑해서 새로운 컨텍스트를 만듦, 각각의 테스트를 묶어 그룹핑
 // (it 함수는 최상위, 독립적인 개별 컨텍스트를 가짐)
-it('기본 placeholeder "텍스트를 입력해 주세요."가 노출된다.', async () => {
-  await render(<TextField />);
+describe('placeholder', () => {
+  // 독립적으로 변수나 함수를 선언해 사용할 수 있음
+  it('기본 placeholeder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    await render(<TextField />);
 
-  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요');
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
-  // matcher: 값이 일치하는지 확인하는 것, 커스텀 가능
-  expect(textInput).toBeInTheDocument();
-});
+    // matcher: 값이 일치하는지 확인하는 것, 커스텀 가능
+    expect(textInput).toBeInTheDocument();
+  });
 
-it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
-  await render(<TextField placeholder="abc" />);
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    await render(<TextField placeholder="abc" />);
 
-  const textInput = screen.getByPlaceholderText('abc');
+    const textInput = screen.getByPlaceholderText('abc');
 
-  expect(textInput).toBeInTheDocument();
+    expect(textInput).toBeInTheDocument();
+  });
 });
